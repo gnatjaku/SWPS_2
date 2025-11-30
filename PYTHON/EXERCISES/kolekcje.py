c7afa189b78b63dd1e6eb9e2f57b635b417431c6
albim72 <marcin.albiniak72@gmail.com>
2025-11-30 14:56:01 +0100

    Create kolekcje.py

---
+++ PYTHON/EXERCISES/kolekcje.py
@@ -0,0 +1,5 @@
+# --- Przykład: system zarządzania biblioteką w Pythonie
+
+# List(list) -> lista książek w bibliotece
+# CTRL + D -> powielanie linii/bloku kodu
+# CTRL + /  -> komentowane/odkomentowanie bloku kodu
