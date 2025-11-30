5fd9bc973d4ea4f92beab5e9f7bfd01c844ea3dd
albim72 <marcin.albiniak72@gmail.com>
2025-11-30 15:00:42 +0100

    Update kolekcje.py

--- PYTHON/EXERCISES/kolekcje.py
+++ PYTHON/EXERCISES/kolekcje.py
@@ -3,3 +3,11 @@
 # List(list) -> lista książek w bibliotece
 # CTRL + D -> powielanie linii/bloku kodu
 # CTRL + /  -> komentowane/odkomentowanie bloku kodu
+
+books = [
+    ("Wiedźmin","Andrzej Sapkowski",1990),
+    ("Pan Tadeusz","Adam Mickiewicz",1834),
+    ("Lalka","Bolesław Prus",1896),
+    ("Quo Vadis","Henryk Sienkiewicz",1890),
+    ("Hobbit","JRR Tolkien",1927)
+]
